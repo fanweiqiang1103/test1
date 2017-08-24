f937308625ac5d89ccc2b6ac6afb52634203f01c
fanweiqiang1103 <30787968+fanweiqiang1103@users.noreply.github.com>
2017-08-24 10:38:10 +0800

    创建person.java
    第一次使用github创建项目文件

---
+++ test1.java
@@ -0,0 +1,21 @@
+class Person{
+  private int age;
+  private boolean sex;
+  private int weight;
+  public void setAge(int age){
+    this.age=age;
+  }
+  public int getAge(){
+    return age;
+  }
+  public void setSex(boolean sex){
+    this.sex = sex;
+  }
+  public boolean getSex(){
+    return sex;
+  }
+  public void setWeight(int weight){
+    this.weight = weight;
+  }
+  
+}
